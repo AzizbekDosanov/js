aee4c02b9d6badf2dc09c6607af871bb66e664a9
Azizbek Do'sanov <dusanovon1@gmail.com>
2022-11-21 15:09:07 +0500

    js lesson

---
+++ app.js
@@ -0,0 +1,6 @@
+let name ="Azizbek"
+let surname="Do`sanov"
+let age ="17"
+let academy="OWA"
+
+console.log("my name is "+name+" my surname is",surname+" I'M "+age+"yers old"+" I study in the " + academy)
